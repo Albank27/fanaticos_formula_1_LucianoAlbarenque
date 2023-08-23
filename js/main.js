a9fe2162d66b872abef3bf05791db179edc57750
Albank27 <luciano.n.albarenque@gmail.com>
2023-08-23 11:22:41 -0300

    agrego function de ranking para mostrar un ranking cuando terminan de contestar las preguntas, todavia no entiendo bien como hacer para acomular la informacion de las respuestas

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//questions "este array es el principal que tiene todas las preguntas y sus detalles(opciones y respuestas)"
+
 const questions = [
 {question: "¿Cuál es el piloto con más campeonatos de Fórmula 1?",
 options: ["Lewis Hamilton", "Michael Schumacher", "Juan Manuel Fangio"],
@@ -43,79 +45,132 @@ answer: "Minardi"
   
   let currentQuestionIndex = 0;
   let correctAnswers = 0;
+  let playerName = "";
   
-function showQuestion() {
-const main = document.getElementById("main");
-main.innerHTML = "";
+  function showQuestion() {
+    const main = document.getElementById("main");
+    main.innerHTML = "";
   
-const question = questions[currentQuestionIndex];
-const questionDiv = document.createElement("div");
-questionDiv.innerHTML = `<p>${question.question}</p>`;
+    if (!playerName) {
+      const playerNameInput = document.createElement("input");
+      playerNameInput.type = "text";
+      playerNameInput.id = "nombre";
+      playerNameInput.placeholder = "Ingresa tu nombre";
   
-for (let j = 0; j < question.options.length; j++) {
-      const option = question.options[j];
-      const radioInput = document.createElement("input");
-      radioInput.type = "radio";
-      radioInput.name = "options";
-      radioInput.value = option;
-      questionDiv.appendChild(radioInput);
-      questionDiv.appendChild(document.createTextNode(option));
-      questionDiv.appendChild(document.createElement("br"));
-    }
-  
-const submitButton = document.createElement("button");
-submitButton.innerText = "Siguiente";
-submitButton.onclick = function () {
-      const selectedAnswer = document.querySelector('input[name="options"]:checked');
-      if (selectedAnswer) {
-        const userAnswer = selectedAnswer.value;
-        if (userAnswer === question.answer) {
-          correctAnswers++;
-        }
-        currentQuestionIndex++;
-        if (currentQuestionIndex < questions.length) {
-          showQuestion();
+      const startButton = document.createElement("button");
+      startButton.innerText = "Comenzar Cuestionario";
+      startButton.onclick = function () {
+        playerName = document.getElementById("nombre").value;
+        if (!playerName) {
+          alert("Por favor, ingresa tu nombre antes de comenzar el cuestionario");
+          //pongo un alert porque sino ingresan el nombre no pueden avanzar en el cuestionario
         } else {
-          showResults();
+          // Ocultar el campo de nombre y mostrar las preguntas
+          playerNameInput.style.display = "none";
+          startButton.style.display = "none";
+          showQuestion(); // Llama a la función para mostrar las preguntas
         }
-      }
-    };
+      };
   
-    main.appendChild(questionDiv);
-    main.appendChild(submitButton);
+      main.appendChild(playerNameInput);
+      main.appendChild(startButton);
+    } else {
+      const question = questions[currentQuestionIndex];
+      const questionDiv = document.createElement("div");
+      questionDiv.innerHTML = `<p>${question.question}</p>`;
+  
+      for (let j = 0; j < question.options.length; j++) {
+        const option = question.options[j];
+        const radioInput = document.createElement("input");
+        radioInput.type = "radio";
+        radioInput.name = "options";
+        radioInput.value = option;
+        questionDiv.appendChild(radioInput);
+        questionDiv.appendChild(document.createTextNode(option));
+        questionDiv.appendChild(document.createElement("br"));
+      }
+  
+      const submitButton = document.createElement("button");
+      submitButton.innerText = "Siguiente";
+      submitButton.onclick = function () {
+        const selectedAnswer = document.querySelector('input[name="options"]:checked');
+        if (selectedAnswer) {
+          const userAnswer = selectedAnswer.value;
+          if (userAnswer === question.answer) {
+            correctAnswers++;
+          }
+          currentQuestionIndex++;
+          if (currentQuestionIndex < questions.length) {
+            showQuestion();
+          } else {
+            showResults();
+          }
+        }
+      };
+  
+      main.appendChild(questionDiv);
+      main.appendChild(submitButton);
+    }
   }
   
-function showResults() {
-const main = document.getElementById("main");
-main.innerHTML = "";
+  function showResults() {
+    const main = document.getElementById("main");
+    main.innerHTML = "";
   
-const resultMessage = document.createElement("div");
-resultMessage.classList.add("alert-message");
+    const resultMessage = document.createElement("div");
+    resultMessage.classList.add("alert-message");
   
-if (correctAnswers >= 8) {
-resultMessage.textContent = "¡Felicidades! Sos crack, sabes de Formula 1";
-} else if (correctAnswers >= 5) {
-resultMessage.textContent = "Mmm maso maso, tenes que seguir intentando para ser bueno.";
-} else {
-resultMessage.textContent = "Patetico, no sabes de formula 1";
-}
-const scoreMessage = document.createElement("div");
-scoreMessage.classList.add("alert-result");
-scoreMessage.innerHTML = `Respuestas correctas: <span class="result-correct">${correctAnswers}</span>/10`;
+    if (correctAnswers >= 8) {
+      resultMessage.textContent = "¡Felicidades! Eres un experto en Fórmula 1";
+    } else if (correctAnswers >= 5) {
+      resultMessage.textContent = "Mmm, conoces algo de Fórmula 1";
+    } else {
+      resultMessage.textContent = "Patetico,no sabes de Fórmula 1";
+    }
+    const scoreMessage = document.createElement("div");
+    scoreMessage.classList.add("alert-result");
+    scoreMessage.innerHTML = `Respuestas correctas: <span class="result-correct">${correctAnswers}</span>/10`;
   
-main.appendChild(resultMessage);
-main.appendChild(scoreMessage);
+    main.appendChild(resultMessage);
+    main.appendChild(scoreMessage);
   
-const restartButton = document.createElement("button");
-restartButton.innerText = "Reiniciar Cuestionario";
-restartButton.onclick = function () {
-currentQuestionIndex = 0;
-correctAnswers = 0;
-showQuestion();
-};
+    // Ya mostre el resultado,ahora doy la opcion de mostrar el ranking
+    const showRankingButton = document.createElement("button");
+    showRankingButton.innerText = "Ver Ranking";
+    showRankingButton.onclick = function () {
+      main.innerHTML = ""; 
+      showRanking(); 
+    };
   
-main.appendChild(restartButton);
-}
-
-//cuando recargo la pagina que se muestre el formulario
-showQuestion();
+    main.appendChild(showRankingButton);
+  }
+  
+  function showRanking() {
+    const main = document.getElementById("main");
+    main.innerHTML = "";
+  
+    // Quiero mostrar un ranking cuando termine el cuestionario 
+    const table = document.createElement("table");
+    const tableHeader = document.createElement("thead");
+    const headerRow = document.createElement("tr");
+    const playerNameHeader = document.createElement("th");
+    playerNameHeader.textContent = "Nombre del Jugador";
+    const playerScoreHeader = document.createElement("th");
+    playerScoreHeader.textContent = "Puntaje";
+  
+    headerRow.appendChild(playerNameHeader);
+    headerRow.appendChild(playerScoreHeader);
+    tableHeader.appendChild(headerRow);
+    table.appendChild(tableHeader);
+  
+    const tableBody = document.createElement("tbody");
+  
+    // Esto es para agregar el resultado del jugador actual al ranking, lo vi en youtube pero todvia no entiendo bien como funciona
+    tableBody.innerHTML = `<tr><td>${playerName}</td><td>${correctAnswers}</td></tr>`;
+  
+    table.appendChild(tableBody);
+    main.appendChild(table);
+  }
+  
+  // Iniciar el cuestionario al cargar la pagina
+  showQuestion();
